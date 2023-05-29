7c89548853f3afad105a05573b7e674ecbab67e0
Fitron Ansori <fitronansori7@gmail.com>
2023-05-29 23:15:29 +0800

    fixed workout routes

--- backend/routes/workoutRoutes.js
+++ backend/routes/workoutRoutes.js
@@ -2,24 +2,25 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Workout Routes");
-});
+router
+  .route("/")
+  .get((req, res) => {
+    res.send("Workout Routes");
+  })
+  .post((req, res) => {
+    res.send("Create Workout Route");
+  });
 
-router.get("/:id", (req, res) => {
-  res.send("Workout Routes by ID");
-});
-
-router.post("/", (req, res) => {
-  res.send("Create Workout Route");
-});
-
-router.patch("/:id", (req, res) => {
-  res.send("Update Workout Route");
-});
-
-router.delete("/:id", (req, res) => {
-  res.send("Delete Workout Route");
-});
+router
+  .route("/:id")
+  .get((req, res) => {
+    res.send("Workout Routes by ID");
+  })
+  .patch((req, res) => {
+    res.send("Update Workout Route");
+  })
+  .delete((req, res) => {
+    res.send("Delete Workout Route");
+  });
 
 module.exports = router;
