4578711a9ff7d339ab48eb0e67e2932ac9c01f88
Fitron Ansori <fitronansori7@gmail.com>
2023-05-30 08:44:29 +0800

    add data models schema

--- backend/config/DBConnetion.js
+++ backend/config/DBConnetion.js
@@ -7,6 +7,7 @@ const connectDB = async () => {
     mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      dbName: "workoutbuddy",
     });
   } catch (err) {
     console.error(err);
