03c1c8161209b8d6f8fa71a556fe7e231375127e
Fitron Ansori <fitronansori7@gmail.com>
2023-05-31 11:15:46 +0800

    add workout delete

--- frontend/src/components/WorkoutDetail.jsx
+++ frontend/src/components/WorkoutDetail.jsx
@@ -1,6 +1,23 @@
 import PropTypes from "prop-types";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContex";
 
 const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext();
+
+  const handleDelete = async () => {
+    const response = await fetch(
+      "http://localhost:2001/api/workouts/" + workout._id,
+      {
+        method: "DELETE",
+      }
+    );
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
+    }
+  };
+
   return (
     <div className="workout-details">
       <h4>{workout.title}</h4>
@@ -13,6 +30,7 @@ const WorkoutDetails = ({ workout }) => {
         {workout.reps}
       </p>
       <p>{workout.createdAt}</p>
+      <span onClick={handleDelete}>Delete</span>
     </div>
   );
 };
