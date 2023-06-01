7ffe63fa93dbef58126a086bffc3729c408212f4
Fitron Ansori <fitronansori7@gmail.com>
2023-06-01 22:47:14 +0800

    add auth-redux

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import workoutsReducer from "../features/workouts/workoutsSlice";
+import authReducer from "../features/auth/authSlice";
 
 export const store = configureStore({
   reducer: {
     workouts: workoutsReducer,
+    auth: authReducer,
   },
 });
