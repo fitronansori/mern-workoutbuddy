501ae744143d12eafcf95eebf5fa5c60810f92e7
Fitron Ansori <fitronansori7@gmail.com>
2023-06-01 22:34:36 +0800

    add jwt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,25 @@
 const mongoose = require("mongoose");
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.SECRET_TOKEN, {
+    expiresIn: "3d",
+  });
+};
 
 // @desc    Get all users
 // @route   GET /api/user
 // @access  Public
 
 const getAllUsers = async (req, res) => {
-  res.send("Get All Users");
+  try {
+    const users = await User.find({});
+
+    res.json(users);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 // @desc    Login user
@@ -14,7 +27,17 @@ const getAllUsers = async (req, res) => {
 // @access  Public
 
 const loginUser = async (req, res) => {
-  res.send("Login User");
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+
+    const token = createToken(user._id);
+
+    res.status(201).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 // @desc    Signup user
@@ -27,7 +50,9 @@ const signupUser = async (req, res) => {
   try {
     const user = await User.signup(email, password);
 
-    res.status(201).json(user);
+    const token = createToken(user._id);
+
+    res.status(201).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const UserSchema = new mongoose.Schema({
   email: {
@@ -13,7 +14,20 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
+// static method untuk login user
 UserSchema.statics.signup = async function (email, password) {
+  if (!email || !password) {
+    throw new Error("Email and password are required");
+  }
+
+  if (!validator.isEmail(email)) {
+    throw new Error("Email is not valid");
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw new Error("Password is not strong enough");
+  }
+
   const emailExist = await this.findOne({ email });
 
   if (emailExist) {
@@ -24,6 +38,32 @@ UserSchema.statics.signup = async function (email, password) {
   const hashedPassword = await bcrypt.hash(password, salt);
 
   const user = await this.create({ email, password: hashedPassword });
+
+  return user;
+};
+
+// static method untuk login user
+
+UserSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw new Error("Email and password are required");
+  }
+
+  if (!validator.isEmail(email)) {
+    throw new Error("Email is not valid");
+  }
+
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw new Error("Email or password is wrong");
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error("Email or password is wrong");
+  }
 
   return user;
 };
