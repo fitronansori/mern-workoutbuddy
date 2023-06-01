6ba7208f143f44722281dae032ed53177f2658d6
Fitron Ansori <fitronansori7@gmail.com>
2023-06-02 00:16:32 +0800

    add auth signup

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,7 +1,26 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
+const url = "http://localhost:2001/api/user";
+
+export const signup = createAsyncThunk(
+  "/signup",
+  async ({ email, password }, thunkAPI) => {
+    try {
+      const response = await axios.post(`${url}/signup`, {
+        email,
+        password,
+      });
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
 const initialState = {
   user: null,
+  status: "idle", // loading, succeeded, failed
+  error: null,
 };
 
 export const authSlice = createSlice({
@@ -16,7 +35,26 @@ export const authSlice = createSlice({
       state.token = null;
     },
   },
+  extraReducers(builder) {
+    builder
+      .addCase(signup.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(signup.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.user = action.payload;
+        localStorage.setItem("user", JSON.stringify(action.payload));
+      })
+      .addCase(signup.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload;
+      });
+  },
 });
+
+export const selectUser = (state) => state.auth.user;
+export const selectStatus = (state) => state.auth.status;
+export const selectError = (state) => state.auth.error;
 
 export const { login, logout } = authSlice.actions;
 export default authSlice.reducer;
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,14 +1,30 @@
 import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  // selectUser,
+  selectStatus,
+  selectError,
+  signup,
+} from "../features/auth/authSlice";
 
 const SignUp = () => {
+  const dispatch = useDispatch();
+
+  // const user = useSelector(selectUser);
+  const status = useSelector(selectStatus);
+  const error = useSelector(selectError);
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
+    dispatch(signup({ email, password }));
 
-    console.log(email, password);
+    setEmail("");
+    setPassword("");
   };
+
   return (
     <form className="signup" onSubmit={handleSubmit}>
       <h3>Sign Up</h3>
@@ -27,6 +43,23 @@ const SignUp = () => {
       />
 
       <button>Sign up</button>
+      <div>
+        {status === "loading" && <p>Loading...</p>}
+        {status === "failed" && (
+          <div
+            style={{
+              padding: "10px",
+              border: "1px solid #e7195a",
+              background: "#ffefef",
+              color: "#e7195a",
+              borderRadius: "5px",
+              margin: "20px 0",
+            }}
+          >
+            <p>{error.error}</p>
+          </div>
+        )}
+      </div>
     </form>
   );
 };
